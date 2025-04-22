e5866c735b2d6ac82f879d03c861d9384eb34de1
pmpr.dev <pmpr.dev@gmail.com>
2025-04-22 17:24:08 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             680567a9cbb01             |
+    |             6807d08900907             |
     |_______________________________________|
 */
  use Pmpr\Module\Salary\Salary; Salary::symcgieuakksimmu();
--- src/Pmpr/Module/Salary/Profile/Admin/Tab/Tab.php
+++ src/Pmpr/Module/Salary/Profile/Admin/Tab/Tab.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             680567a9cbb01             |
+    |             6807d08900907             |
     |_______________________________________|
 */
  namespace Pmpr\Module\Salary\Profile\Admin\Tab; use Pmpr\Common\Foundation\Page\Admin\Tab as BaseClass; abstract class Tab extends BaseClass { }
